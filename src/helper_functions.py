e8ad50ac7681540bd4967e064be863c6b2c3bc64
Gonzalo Rivero <griverorz@gmail.com>
2013-10-07 21:38:22 -0700

    Rewritten as OO

---
+++ src/helper_functions.py
@@ -0,0 +1,113 @@
+#! /usr/bin/python
+
+def argmax(lst):
+     return lst.index(max(lst))    
+
+def argmin(lst):
+     return lst.index(min(lst))    
+    
+def toInt(numList):
+    # Takes a list and returns it as integer
+    if numList != []:
+        s = ''.join(map(str, numList))
+        return int(s)
+
+def percentile(N, P):
+    """
+    Find the percentile of a list of values
+
+    @parameter N - A list of values.  N must be sorted.
+    @parameter P - A float value from 0.0 to 1.0
+
+    @return - The percentile of the values.
+    """
+    N.sort()
+    n = int(round(P * len(N) + 0.5))
+    return N[n-1]
+    
+def baseconvert(number, todigits, fromdigits = "0123456789"):
+    """ converts a "number" between two bases of arbitrary digits
+
+    The input number is assumed to be a string of digits from the
+    fromdigits string (which is in order of smallest to largest
+    digit). The return value is a string of elements from todigits
+    (ordered in the same way). The input and output bases are
+    determined from the lengths of the digit strings. Negative 
+    signs are passed through.
+    """
+    if str(number)[0]=='-':
+        number = str(number)[1:]
+        neg = 1
+    else:
+        neg = 0
+
+    # make an integer out of the number
+    x = long(0)
+    for digit in str(number):
+        x = x*len(fromdigits) + fromdigits.index(digit)
+    
+    # create the result in base 'len(todigits)'
+    res = ""
+    while x > 0:
+        digit = x % len(todigits)
+        res = todigits[digit] + res
+        x /= len(todigits)
+    if neg:
+        res = "-"+res
+    if number is 0:
+        res = "0"
+    return res
+
+def product(vv):
+   return(reduce(lambda x, y: x*y, vv))
+
+def generate_first_code(toprank, level):
+    """ 1 is lowest rank """
+    ll = (toprank - level) + 1
+    out = []
+    for i in range(ll):
+        out.append(10**i)
+    return sum(out)
+
+def generate_base_codes(toprank, unitsize):
+    list_codes = []
+    start = generate_first_code(toprank, 1)
+    pop_rank = unitsize**toprank
+        
+    for i in range(pop_rank):
+        id = baseconvert(i, [str(j) for j in range(unitsize)])
+        list_codes.append(start + int(id))
+    return(list_codes)
+
+def populate_army(topage, toprank, unitsize):
+    """ Generates army of size N (at the base level) with K levels. 
+        Each unit being of size U
+    """
+    army = []
+    fill_rank = 1
+    while fill_rank <= toprank:
+        
+        start = generate_first_code(toprank, fill_rank)
+        pop_rank = unitsize**(toprank - fill_rank + 1)
+        
+        for i in range(pop_rank):
+            id = baseconvert(i, [str(j) for j in range(unitsize)])
+            ## Senior officers tend to be older
+            refbase = (toprank + 1) - fill_rank
+            refscale = topage - 1
+            age = int(round(np.random.beta(fill_rank, refbase, 1) * refscale + 1))
+            seniority = random.choice(range(min(age, fill_rank), 
+                                            max(age, fill_rank) + 1))
+            quality = random.uniform(0, 1)
+            ## Put everything together
+            captain = Soldier(fill_rank, 
+                              seniority,
+                              age,
+                              quality, 
+                              random.uniform(-1, 1),
+                              start + int(id))
+            army.append(captain)
+        pop_rank = pop_rank/unitsize
+        fill_rank += 1
+    return army
+
---
+++ src/main_classes.py
@@ -0,0 +1,252 @@
+#! /usr/bin/python
+
+""" MAIN CLASSES """
+
+import helper_functions
+import sys
+import random
+import itertools
+import numpy as np
+from collections import defaultdict
+from copy import deepcopy
+
+class Soldier(object):
+    """ A soldier """
+
+    id_generator = itertools.count(1)
+    
+    def __init__(self, rank, seniority, age, quality, ideology, unit):
+        self.id = next(self.id_generator)
+        self.rank = rank
+        self.seniority = seniority
+        self.age = age
+        self.quality = quality
+        self.ideology = ideology
+        self.unit = unit
+        self.alive = True
+
+    def __str__(self):
+        chars = 'ID: '+ str(self.id) +                    \
+                '\nRank: ' +  str(self.rank) +            \
+                '\nSeniority: ' + str(self.seniority) +   \
+                '\nAge: ' + str(self.age) +               \
+                '\nQuality: ' + str(self.quality) +       \
+                '\nIdeology: ' + str(self.ideology) +     \
+                '\nUnit: ' + str(self.unit)
+        return chars
+        
+    def report(self):
+        return({'id': self.id,
+                'rank': self.rank,
+                'age': self.age,
+                'quality': self.quality,
+                'ideology': self.ideology,
+                'unit': self.unit,
+                'seniority': self.seniority})
+    
+    def pass_time(self, topage):
+        self.age += 1
+        self.seniority += 1
+        if self.age > topage:
+            self.kill()
+        elif self.rank is 1:
+            prob = float(self.age)/float(topage + 1)
+            to_kill = int(np.random.binomial(1, prob, 1))
+            if to_kill is 1:
+                self.kill()
+        
+    def will_retire(self, topage):
+        if self.age == topage:
+            return(True)
+        else:
+            return(False)
+    
+    def is_candidate(self, ordered, rank_open, topage, slack = 1):
+        isc = False
+        if ordered is False:
+            if self.rank < rank_open and self.age < topage and self.alive:
+                isc = True
+        elif ordered is True:
+            if self.rank == (rank_open - slack) and self.age < topage and self.alive:
+                isc = True
+        return isc
+        
+    def promote(self, rank_open = None):
+        if rank_open is None:
+            self.rank += 1
+        else:
+            self.rank = rank_open
+        self.seniority = 0
+
+    def kill(self):
+        self.alive = False
+
+    def reuse(self, unit):
+        self.age = int(round(np.random.gamma(1, 2, 1)))
+        self.rank = 1
+        self.quality = random.uniform(0, 1)
+        self.ideology = random.uniform(-1, 1)
+        self.seniority = 1
+        self.unit = unit
+        self.id = next(self.id_generator)
+        self.alive = True
+
+
+class Ruler(object):
+    """ The ruler """
+    def __init__(self, ideology):
+        self.ideology = ideology
+
+    def __str__(self):
+        characteristics = "\nIdeology: " + str(self.ideology)
+        return characteristics
+
+
+class Army(Soldier):
+    """ A collection of soldiers """
+
+    def __init__(self, ruler, top_age, unit_size, top_rank):
+        """ Generates army of size N (at the base level) with K levels. 
+        Each unit being of size U """
+        self.N = unit_size**top_rank
+        self.top_age = top_age
+        self.unit_size = unit_size
+        self.top_rank = top_rank
+        self.ruler_ideology = ruler.ideology
+        if self.N % self.unit_size is not 0:
+            print('Wrong dimensions')
+        self.soldiers = populate_army(self.top_age, self.top_rank, self.unit_size)
+
+    def __str__(self):
+        chars = "Soldiers: " + str(self.N) + \
+                "\nTop age: " + str(self.top_age) + \
+                "\nUnit size: " + str(self.unit_size) + \
+                "\nTop rank: " + str(self.top_rank)
+        return chars
+        
+    def get_rank(self, rank):
+        rank_list = [i for i in self.soldiers if i.rank == rank]
+        return(rank_list)
+
+    def rank_dist(self):
+        ranks = [i.rank for i in self.soldiers if i.alive]
+        rankd = defaultdict(int)
+        for ind in ranks:
+            rankd[ind] += 1
+        return(rankd)
+
+    def up_for_retirement(self):
+        avail_list = []
+        topage = self.top_age
+        for i in self.soldiers:
+            avail_list.append(i.will_retire(topage))
+        retirees = [val for pos, val in enumerate(self.soldiers) 
+                    if avail_list[pos] and val.rank > 1]        
+        ## sort them by rank
+        retirees = sorted(retirees, key = lambda x: x.rank, reverse = True)
+        return(retirees)
+
+    def up_for_promotion(self, constraints, open_rank, slack = 1):
+        ### methods
+        # random, quality, proximity, seniority
+        ### constraints
+        # ordered
+        known_constraints = {
+            ## ordered, seniority
+            'ordered': True,
+            'none': False
+        }
+        ordered = known_constraints[constraints]
+        pool_list = []
+        topage = self.top_age
+        for i in self.soldiers:
+            pool_list.append(i.is_candidate(ordered, open_rank, topage, slack))
+        candidates = [val for pos, val in enumerate(self.soldiers) 
+                      if pool_list[pos] is True]
+        return(candidates)            
+
+    def lookupid(self, value):
+        id_army = [i.id for i in self.soldiers]
+        idx = id_army.index(value)
+        return idx
+
+    def promote(self, method, constraints, openpos):
+        unavail = []
+        while len(openpos) > 0:
+            open_rank = openpos[0].rank
+            pool = deepcopy(self.up_for_promotion(constraints, open_rank, slack = 1))
+            pool = list(set(pool).difference(set(unavail)))
+            
+            while not pool:
+                slack = 2
+                print("No one is up! Looking in the next rank.")
+                pool = self.up_for_promotion(constraints, open_rank, slack)
+                slack += 1
+
+            if method is "seniority":
+                refval = max([i.seniority for i in pool])
+                id_pool = [i.id for i in pool if i.seniority is refval][0]
+                idx = self.lookupid(id_pool)
+            if method is "quality":
+                refval = max([i.quality for i in pool])
+                id_pool = [i.id for i in pool if i.quality is refval][0]
+                idx = self.lookupid(id_pool)
+            if method is "ideology":
+                refval = min([(i.ideology - self.ruler_ideology) for i in pool])
+                id_pool = [i.id for i in pool if (i.ideology - self.ruler_ideology) is refval][0]
+                idx = self.lookupid(id_pool)
+            if method is "random":
+                refval = random.choice([i.id for i in pool])
+                ids = [i.id for i in self.soldiers]
+                idx = ids.index(refval)
+
+            tmp = deepcopy(self.soldiers[idx])
+            self.soldiers[idx].promote(open_rank)
+            self.soldiers[idx].unit = openpos[0].unit 
+            unavail.append(self.soldiers[idx])
+
+            openpos.pop(0)
+
+            if tmp.rank > 1:
+                openpos.append(tmp)
+                openpos = sorted(openpos,
+                                       key = lambda x: x.rank,
+                                       reverse = True)
+
+    def reuse_officers(self):
+        dead_soldiers, dead_officers = [], []
+        for i in self.soldiers:
+            if not i.alive:
+                if i.rank is 1:
+                    dead_soldiers.append(i)
+                else:
+                    dead_officers.append(i)
+
+        idle_codes = [i.unit for i in dead_soldiers]
+        
+        for i, officer in enumerate(dead_officers):
+            idx = self.soldiers.index(officer)
+            self.soldiers[idx].reuse(idle_codes[i])              
+
+    def reuse_soldiers(self):
+        dead_soldiers = []
+        for i in self.soldiers:
+            if not i.alive:
+                if i.rank is 1:
+                    dead_soldiers.append(i)
+
+        idle_codes = [i.unit for i in dead_soldiers]
+        
+        for i, soldier in enumerate(dead_soldiers):
+            idx = self.soldiers.index(soldier)
+            uu = self.soldiers[idx].unit
+            self.soldiers[idx].reuse(uu)
+
+    def recruit(self):
+        self.reuse_officers()
+        self.reuse_soldiers()
+
+    def pass_time(self):
+        for i in self.soldiers:
+            Soldier.pass_time(i, self.top_age)
+    
